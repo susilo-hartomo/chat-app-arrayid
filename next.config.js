e682af3069a0aac30fa1f43bde155e86aff4bcd7
Susilo Hartomo <encusilo545110@gmail.com>
2022-02-23 17:30:15 +0700

    update video neon

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    urlImports: ['https://example.com/modules/'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: 'build',
 }
